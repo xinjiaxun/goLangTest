6989938065721e5d5981794c993070bfc4bde86f
辛佳勋 <1339413169@qq.com>
2021-11-04 17:02:10 +0800

    updated

--- main.go
+++ main.go
@@ -1,6 +1,23 @@
 package main
 
+import "fmt"
+//全局变量
+var name string = "pprof.cn"  //go 定义变量
+var sex int = 1
 
-func main()  {
-	println("Hello World!")
+
+func main ()  {
+
+	age :=16  	//局部变量
+	println(age+sex)
+	println(name)
+	println(sex)
+
+	//数组
+	a1 := [3] int {1, 2, 3}
+	arr1 := [5] string {"1","2","3","4","5"}
+
+	fmt.Println(a1)
+	fmt.Println(arr1)
+
 }
